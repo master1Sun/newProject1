343155e4775d1aa620ae45ba22bbf17d9f232bfc
master1Sun <457358344@qq.com>
2019-09-27 14:44:05 +0800

    Revert "Initial commit"
    This reverts commit 719ef183f48c87840ded6b9b2619331f927f0010.

--- miniprogram/libs/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-  key: '405f13b4e644fc3035d2284eb51f3f3e'
-}
-
-module.exports.Config = config;
--- miniprogram/libs/openapiUrl-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-  getJoke: 'https://api.apiopen.top/getJoke',
-  satinCommentApi: 'https://www.apiopen.top/satinCommentApi',
-  emailSearch: 'https://www.kuaidi100.com/query',
-  urlType : "https://www.kuaidi100.com/autonumber/autoComNum",
-  meituApi: 'https://www.apiopen.top/meituApi?page=0',//美图
-}
-
-module.exports.Config = config;
